c1eb3b9c2baeb438c2299bd3516d17130082e834
Sergey Chepurnov <chepurnov.serge@gmail.com>
2026-09-13 22:53:23 +0700

    feat: search flight on date

--- src/main/java/io/hexlet/flightbooking/service/BookingService.java
+++ src/main/java/io/hexlet/flightbooking/service/BookingService.java
@@ -1,21 +1,11 @@
 package io.hexlet.flightbooking.service;
 
-import io.hexlet.flightbooking.dto.AirlineDto;
-import io.hexlet.flightbooking.dto.BookingDto;
-import io.hexlet.flightbooking.dto.CityDto;
-import io.hexlet.flightbooking.dto.ContactDto;
-import io.hexlet.flightbooking.dto.FlightDto;
-import io.hexlet.flightbooking.dto.MoneyDto;
-import io.hexlet.flightbooking.dto.PassengerDto;
-import io.hexlet.flightbooking.model.Booking;
 import io.hexlet.flightbooking.repository.AirlineRepository;
 import io.hexlet.flightbooking.repository.BookingPassengerRepository;
 import io.hexlet.flightbooking.repository.BookingRepository;
 import io.hexlet.flightbooking.repository.CityRepository;
 import io.hexlet.flightbooking.repository.FlightRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class BookingService {
@@ -34,38 +24,7 @@ public class BookingService {
         this.cities = cities;
     }
 
-    public Optional<BookingDto> findByCodeAndLastName(String code, String lastName) {
-        return bookings.findByCodeAndLastName(code, lastName)
-                .map(this::assemble);
-    }
 
-    private BookingDto assemble(Booking b) {
-        var flight = flights.findById(b.flightId()).orElseThrow();
-        var airline = airlines.findByCode(flight.airlineCode()).orElseThrow();
-        var origin = cities.findByCode(flight.originCode()).orElseThrow();
-        var dest = cities.findByCode(flight.destinationCode()).orElseThrow();
 
-        var passengerDtos = passengers.findByBookingCode(b.code()).stream()
-                .map(p -> new PassengerDto(
-                        p.firstName(), p.lastName(),
-                        p.dateOfBirth(), p.documentNumber()))
-                .toList();
 
-        return new BookingDto(
-                b.code(),
-                b.status(),
-                new FlightDto(
-                        flight.id(), flight.flightNumber(),
-                        new AirlineDto(airline.code(), airline.name()),
-                        new CityDto(origin.code(), origin.name(), origin.country()),
-                        new CityDto(dest.code(), dest.name(), dest.country()),
-                        flight.departureAt(), flight.arrivalAt(),
-                        flight.durationMinutes(),
-                        new MoneyDto(flight.priceAmount(), flight.priceCurrency()),
-                        flight.seatsAvailable()),
-                passengerDtos,
-                new ContactDto(b.contactEmail(), b.contactPhone()),
-                new MoneyDto(b.totalAmount(), b.totalCurrency()),
-                b.createdAt());
-    }
 }
